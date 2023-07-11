5a63240647280f3854840e6e5a19c01b6d0cd58b
Erik Ferreira <erikferreira03@outlook.com>
2023-07-11 10:52:16 -0300

    .

--- desafios/Des001/script.js
+++ desafios/Des001/script.js
@@ -2,13 +2,17 @@ function carregar() {
 var msg = document.getElementById('msg')
 var img = document.getElementById('imagem')
 var data = new Date ()
-var hora = data.getHours()
+//var hora = data.getHours()
+var hora = 20
 msg.innerHTML = `Agora são ${hora} horas`
 if (hora >= 0 && hora < 12){
     img.src = 'imagens/manhã.jpg'
+    document.body.style.background = '#BEA07C'
 }else if (hora >= 12 && hora < 18){
     img.src = 'imagens/tarde.jpg'
+    document.body.style.background = '#74495C'
 }else {
     img.src = 'imagens/noite.jpg'
+    document.body.style.background = '#09213D'
 }
 }
