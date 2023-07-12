2152b636ca95551ab4a05454fa015791df3f664f
Erik Ferreira <erikferreira03@outlook.com>
2023-07-12 18:54:02 -0300

    .

---
+++ desafios/Des002/script.js
@@ -0,0 +1,17 @@
+function carregar() {
+var msg = document.getElementById('msg')
+var img = document.getElementById('imagem')
+var data = new Date ()
+var hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas`
+if (hora >= 0 && hora < 12){
+    img.src = 'imagens/manhã.jpg'
+    document.body.style.background = '#BEA07C'
+}else if (hora >= 12 && hora < 18){
+    img.src = 'imagens/tarde.jpg'
+    document.body.style.background = '#74495C'
+}else {
+    img.src = 'imagens/noite.jpg'
+    document.body.style.background = '#09213D'
+}
+}
