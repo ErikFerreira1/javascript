1326bb866d2b997d5986199bff68236f8314c145
Erik Ferreira <erikferreira03@outlook.com>
2023-07-15 18:52:11 -0300

    .

--- desafios/Des004/script.js
+++ desafios/Des004/script.js
@@ -1,28 +1,9 @@
 function verificar() {
-    var inicio = document.getElementById('inicio')
-    var fim = document.getElementById('fim')
-    var passo = document.getElementById('passo')
-    var res = document.getElementById('res')
-   
-    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        alert('[ERRI] Faltam dados!')
-
+    let número = document.getElementById('número')
+    if(número < 0) {
+        alert('[ERRO] Número inválido')
     } else {
-        res.innerHTML = 'Contando: '
-        let i = Number(inicio.value) 
-        let f = Number(fim.value) 
-        let p = Number(passo.value)
-        if(i < f) {
-            // Contagem crescente
-            for(let c = i; c <= f; c += p) {
-                res.innerHTML += `${c} `
-            }
-        } else {
-            // Contagem regressiva
-            for(let c = i; c <= f; c -= p) {
-                res.innerHTML += `${c} `
-            }
-        }
-        
+        let num = Number(número)
+       
     }
-}   
+}
