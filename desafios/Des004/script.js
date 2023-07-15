6e24f132725159ca05eb6419cad1ea19e93472e7
Erik Ferreira <erikferreira03@outlook.com>
2023-07-15 19:15:31 -0300

    .

--- desafios/Des004/script.js
+++ desafios/Des004/script.js
@@ -1,9 +1,18 @@
 function verificar() {
     let número = document.getElementById('número')
-    if(número < 0) {
+    let tab = document.getElementById('seltab')
+    if(número.value < 0 || número.value.length == 0) {
         alert('[ERRO] Número inválido')
     } else {
-        let num = Number(número)
-       
+        let num = Number(número.value)
+        let c = 1
+        while(cont <= 10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${cont} = ${n*cont}`
+            tab.appendChild(item)
+            c++
+            }
+        }
+
+        
     }
-}
