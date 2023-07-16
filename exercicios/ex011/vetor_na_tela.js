503a3a4b1838dba28c36abfd9ec9f3bfb6a0ee96
Erik Ferreira <erikferreira03@outlook.com>
2023-07-16 16:00:57 -0300

    Update vetor_na_tela.js

--- exercicios/ex011/vetor_na_tela.js
+++ exercicios/ex011/vetor_na_tela.js
@@ -1,1 +1,4 @@
-let num = [ 5, 8, 2, 9, 3 ]
+let num = [ 5, 8, 2, 9, 3 ] // mostrar os valores dentro do array, separadamente 
+for (let cont = 0; cont < num.length; cont++){
+    console.log(num[cont])
+}
