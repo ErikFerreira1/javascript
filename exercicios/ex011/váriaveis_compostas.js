910c253dbe0d96bbc58a0f74bb8285490bf4cbad
Erik Ferreira <erikferreira03@outlook.com>
2023-07-16 15:54:31 -0300

    Create váriaveis_compostas.js

---
+++ exercicios/ex011/váriaveis_compostas.js
@@ -0,0 +1,6 @@
+let num = [ 5, 8, 2, 9, 3 ]
+num.sort() // sortear os números dentro do array
+num.push(1) // colocar um número no final da tabela
+num[3] = 7 
+console.log(num)
+console.log(num.length) // tamanho do array
