91d266d93a0cb37b798c916e9b253b1e7396d511
Erik Ferreira <erikferreira03@outlook.com>
2023-07-17 19:01:47 -0300

    Update script.js

--- desafios/Des005/script.js
+++ desafios/Des005/script.js
@@ -4,13 +4,17 @@ function adicionar() {
     let res = document.getElementById('option')
     if (num.value > 100 || num.value < 1 || num.value.length == 0){
         alert('Digite um valor válido!')
-    } else if(list_num.indexOf(num) == -1) {
-        res.innerText = `Número ${num.value} foi adicionado`
-        list_num.push(num)
     } else {
-        alert(`O número ${num.value} já foi adicionado`)
+        if(list_num.indexOf(num.value) > -1) {
+            alert(`O número ${num.value} já foi adicionado`)
+            
+        } else {
+            list_num.push(num.value)
+            res.innerText = `Número ${num.value} foi adicionado`
+        }
     }
 }
 function verificar() {
-    
+    let info = document.getElementById('info')
+    info.innerHTML = `Ao todo temos, foram adicionados ${list_num.length} números`
 }
