c108316c74307eab7c8325d0c6d76fd428f08cae
Erik Ferreira <erikferreira03@outlook.com>
2023-07-18 22:01:01 -0300

    .

---
+++ exercicios/ex014/DOM_Array_Map.js
@@ -0,0 +1,5 @@
+var numeros = [1, 2, 3, 4] 
+var dobraNumeros = numeros.map(function(numeros){
+    return numeros * 2;
+})
+console.log(dobraNumeros); // A função map pode alterar valores dentro do array
