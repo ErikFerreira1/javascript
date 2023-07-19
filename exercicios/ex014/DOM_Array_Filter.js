f37c9de1ca834bc4c903d4f42a011e1c890f6ae8
Erik Ferreira <erikferreira03@outlook.com>
2023-07-18 22:38:43 -0300

    Create DOM_Array_Filter.js

---
+++ exercicios/ex014/DOM_Array_Filter.js
@@ -0,0 +1,7 @@
+var numeros = [1, 2, 3, 4]
+var AcharElementos = numeros.filter(function(numeros){
+    return numeros > 2
+})
+
+console.log(AcharElementos)
+//filtra algo, pode ser utilizado para fazer barra de pesquisa
