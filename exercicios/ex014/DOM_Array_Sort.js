c1922a6ee37e3e7f6b2a1a30171b965624fec710
Erik Ferreira <erikferreira03@outlook.com>
2023-07-20 14:27:43 -0300

    .

--- exercicios/ex014/DOM_Array_Sort.js
+++ exercicios/ex014/DOM_Array_Sort.js
@@ -1,4 +1,4 @@
 var numbers = [3, 1, 2, 4]
-numbers.sort()
-
-console.log(numbers)
+numbers.sort(function(a,b){ return a - b}) // deixa em ordem crescente ou alfabetica
+numbers.reverse() // deixa em ordem decrescente
+console.log(numbers) 
