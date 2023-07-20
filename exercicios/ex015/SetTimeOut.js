277fe7abef256e3bae9ccf7775127138c505016f
Erik Ferreira <erikferreira03@outlook.com>
2023-07-20 14:51:24 -0300

    .

---
+++ exercicios/ex015/SetTimeOut.js
@@ -0,0 +1,5 @@
+// setTimeOut(function, tempo em milisegundos)
+// Executa uma função, depois de esperar em um número especifico de milisegundos
+// setInterval(function, milisegundos)
+// É o mesmo que o setTimeOut(), mas repete a função contuamente
+
