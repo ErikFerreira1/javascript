3cd384c1a8e60411c072c618f8bd040758598f6d
Erik Ferreira <erikferreira03@outlook.com>
2023-07-20 15:03:12 -0300

    .

--- exercicios/ex015/SetTimeOut.js
+++ exercicios/ex015/SetTimeOut.js
@@ -3,3 +3,17 @@
 // setInterval(function, milisegundos)
 // É o mesmo que o setTimeOut(), mas repete a função contuamente
 
+function clicou() {
+    
+    document.getElementById('teste').innerHTML = 'Começou a contagem'
+    //Executa apenas uma vez após o tempo
+    tempo = setTimeout(function(){
+        document.body.style.backgroundColor = 'lightblue'
+    }, 3000) // tempos após a função 
+}
+
+function parou(){
+    clearTimeout(tempo)
+    document.getElementById('teste').innerHTML = 'Parou a contagem'
+    document.body.style.backgroundColor = 'white'
+}
