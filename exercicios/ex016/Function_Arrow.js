a05128f6c60c9f0bb93d14997b712a3adbbd8f7c
Erik Ferreira <erikferreira03@outlook.com>
2023-07-21 19:43:35 -0300

    Create Function_Arrow.js

---
+++ exercicios/ex016/Function_Arrow.js
@@ -0,0 +1,19 @@
+/*
+function isAdult(age) {
+    return age >= 18
+}
+*/
+
+const isAdult = (age) => age >= 18 //Retirasse a function, chaves e return
+
+
+/*
+document.addEventListener('click', function(){
+    console.log('Clicked')
+})
+
+*/
+
+document.addEventListener('click', () => {
+    console.log('Clicked')
+})//caso tenha mais de uma ação, colocasse as chaves após a function
