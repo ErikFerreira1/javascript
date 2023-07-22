e83c265f7374e12966463df7d10b951cdaee0bc4
Erik Ferreira <erikferreira03@outlook.com>
2023-07-21 21:22:37 -0300

    .

--- exercicios/ex018/script.js
+++ exercicios/ex018/script.js
@@ -1,12 +1,24 @@
 const objs = [
-    {
-    "nome": "Erik", 
-    "idade": 20,
-    "esta_trabalhando": true,
-    "detalhes_profissao": {
-        "profissão": "Programador",
-        "empresa": "Empresa x"
+  {
+    nome: "Erik",
+    idade: 20,
+    esta_trabalhando: true,
+    detalhes_profissao: {
+      profissão: "Programador",
+      empresa: "Empresa x",
     },
-    "hobbies": ["Academia", "Programar", "jogar"]
-    }
+    hobbies: ["Academia", "Programar", "jogar"],
+  },
 ]
+
+// Trasnformar objetos em JSON
+const jsonData = JSON.stringify(objs) 
+
+console.log(jsonData)
+console.log(typeof(objs))
+
+// Transformar JSON em objetos
+const objData = JSON.parse(jsonData)
+
+console.log(objData)
+console.log(typeof(objData))
