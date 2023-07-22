07f42243d7e3d274b9baa33f98a92dac7365a190
Erik Ferreira <erikferreira03@outlook.com>
2023-07-21 21:07:12 -0300

    .

---
+++ exercicios/ex018/script.js
@@ -0,0 +1,12 @@
+const objs = [
+    {
+    "nome": "Erik", 
+    "idade": 20,
+    "esta_trabalhando": true,
+    "detalhes_profissao": {
+        "profissão": "Programador",
+        "empresa": "Empresa x"
+    },
+    "hobbies": ["Academia", "Programar", "jogar"]
+    }
+]
