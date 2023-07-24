aef83bcb8f79192de2b39fca9a2b971a4e131b12
Erik Ferreira <erikferreira03@outlook.com>
2023-07-24 14:19:23 -0300

    Update TemplateExpressions.jsx

--- react/Curso React/src/components/TemplateExpressions.jsx
+++ react/Curso React/src/components/TemplateExpressions.jsx
@@ -1,9 +1,14 @@
 const TemplateExpressions = ()  => {
    
    const name = "Erik"
+   const date = {
+        age: 20,
+        profissão: "Programador"
+   }
     return (
         <div>
             <h1>Olá, {name}!</h1>
+            <h1>Você é {date.profissão}</h1> {/*Utilização de JS dentro do JSX*/}
         </div>
     )
 }
