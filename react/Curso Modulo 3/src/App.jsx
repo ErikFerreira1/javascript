b7e44d2bc1ec8a821004c5b091a7c358142e693a
Erik Ferreira <erikferreira03@outlook.com>
2023-07-24 20:17:37 -0300

    Update App.jsx

--- react/Curso Modulo 3/src/App.jsx
+++ react/Curso Modulo 3/src/App.jsx
@@ -1,14 +1,18 @@
-
-import './App.css'
+import Img1 from "./assets/img1.jpg"; // Imagem em p
+import "./App.css";
 
 function App() {
-  
-
   return (
-    <div className='App'>
-        <h1>Avançando em REACT!</h1>
+    <div className="App">
+      <h1>Avançando em REACT!</h1>
+      <div>
+        <img src="/img2.jpg" alt="floresta" /> {/* imagem em public*/}
+      </div>
+      <div>
+        <img src={Img1} alt="praia" /> {/* Imagem em assets*/}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
