58c405681797c6d36c56d0567f5470ffa6913fe7
Erik Ferreira <erikferreira03@outlook.com>
2023-07-25 20:16:37 -0300

    Update ConditionalRender.jsx

--- react/Curso Modulo 3/src/components/ConditionalRender.jsx
+++ react/Curso Modulo 3/src/components/ConditionalRender.jsx
@@ -1,28 +1,35 @@
-import { useState } from "react"
-
+import { useState } from "react";
 
 const ConditionalRender = () => {
-    const [x] = useState(true) // estado do x
-    const [name] = useState("João")
+  const [x] = useState(true); // estado do x
+  const [name] = useState("João");
   return (
     <div>
-        <h1>Isso será exibido?</h1>
-        {x && <p>Se x for true, sim! </p>} {/* Se x for verdadeiro */}
-        {!x && <p>Agora x é falso!</p>}{/* Se x for falso */}
-        <h1>if ternário</h1>
-        {name === "Erik" ? (
-            <div>
-                <p>O nome é João</p>
-            </div>
-        ):(
-            <div>
-                <p>Nome não encontrado</p>
-                <button onClick={() => {name = useState("João")}}>Clique</button>
-            </div>
-
-        )}
+      <h1>Isso será exibido?</h1>
+      {x && <p>Se x for true, sim! </p>} {/* Se x for verdadeiro */}
+      {!x && <p>Agora x é falso!</p>}
+      {/* Se x for falso */}
+      <h1>if ternário</h1>
+      {name === "Erik" ? (
+        <div>
+          <p>O nome é João</p>
+        </div>
+      ) : (
+        //if e else
+        // estrutura => ()? ():()
+        <div>
+          <p>Nome não encontrado</p>
+          <button
+            onClick={() => {
+              name = useState("João");
+            }}
+          >
+            Clique
+          </button>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ConditionalRender
+export default ConditionalRender;
