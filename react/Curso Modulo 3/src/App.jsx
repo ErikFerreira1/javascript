9dea0e4d3148b9afdf0cfac4b0a6b51c8a428d21
Erik Ferreira <erikferreira03@outlook.com>
2023-07-25 19:37:39 -0300

    Update App.jsx

--- react/Curso Modulo 3/src/App.jsx
+++ react/Curso Modulo 3/src/App.jsx
@@ -2,7 +2,6 @@ import Img1 from "./assets/img1.jpg"; // Imagem em assets
 import "./App.css";
 import ManageData from "./components/ManageData";
 import ListRender from "./components/ListRender";
-
 function App() {
   return (
     <div className="App">
@@ -13,8 +12,8 @@ function App() {
       <div>
         <img src={Img1} alt="praia" /> {/* Imagem em assets*/}
       </div>
-      <ManageData/>
-      <ListRender/>
+      <ManageData />
+      <ListRender />
     </div>
   );
 }
