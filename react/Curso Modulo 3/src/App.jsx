cbad065b3c3812d7133ff0908d56e4a2867cc818
Erik Ferreira <erikferreira03@outlook.com>
2023-07-26 19:58:53 -0300

    .

--- react/Curso Modulo 3/src/App.jsx
+++ react/Curso Modulo 3/src/App.jsx
@@ -6,9 +6,15 @@ import ConditionalRender from "./components/ConditionalRender";
 import ShowUserName from "./components/ShowUserName";
 import { useState } from "react";
 import CarDetails from "./components/CarDetails";
+import Fragments from "./components/Fragments";
 function App() {
   const name = "José";
   const [nome] = useState("Maria");
+  const cars = [
+    { id: 1, Marca: "KIA", Cor: "Branco", Km: 12121, NewCar: false },
+    { id: 1, Marca: "Renauld", Cor: "Azul", Km: 5497, NewCar: false },
+    { id: 1, Marca: "Volvo", Cor: "Preto", Km: 0, NewCar: true },
+  ];
   return (
     <div className="App">
       <h1>Avançando em REACT!</h1>
@@ -21,11 +27,24 @@ function App() {
       <ManageData />
       <ListRender />
       <ConditionalRender />
-      <ShowUserName name={nome} />{/* Três formas de passar o parâmetro. name = "", name={nome} ou com useStates  */}
-      <CarDetails Marca="BMW" Km={10000} Cor="Azul" NewCar={false} />{/* passa o parâmetro na Apps.jsx */}
-      <CarDetails Marca="VW" Km={4500} Cor="Vermelho" NewCar={false} />{/* Pode reaproveitar classes. */}
-      <CarDetails marca="Ferrari" Km={0} Cor="Amarelo" NewCar={true} />
-      
+      <ShowUserName name={nome} />
+      {/* Três formas de passar o parâmetro. name = "", name={nome} ou com useStates  */}
+      <CarDetails Marca="BMW" Km={10000} Cor="Azul" NewCar={false} />
+      {/* passa o parâmetro na Apps.jsx */}
+      <CarDetails Marca="VW" Km={4500} Cor="Vermelho" NewCar={false} />
+      {/* Pode reaproveitar classes. */}
+      <CarDetails Marca="Ferrari" Km={0} Cor="Amarelo" NewCar={true} />
+
+      {/* Loop usando todas as funções. */}
+      {cars.map((car) => (
+        <CarDetails
+          Marca={car.Marca}
+          Km={car.Km}
+          Cor={car.Cor}
+          NewCar={car.NewCar}
+        />
+      ))}
+      <Fragments />
     </div>
   );
 }
---
+++ react/Curso Modulo 3/src/components/Fragments.jsx
@@ -0,0 +1,13 @@
+
+
+const Fragments = () => {
+  return (
+    <> {/* Utilizasse para não criar uma div desnecessária no HTML, apenas vai a tag dentro */}
+        <h1>
+            Teste de fragments
+        </h1>
+    </>
+  )
+}
+
+export default Fragments
