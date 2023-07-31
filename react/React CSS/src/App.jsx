c68d13bc64af24b59b8accc82142a8b90431ef73
Erik Ferreira <erikferreira03@outlook.com>
2023-07-31 19:06:59 -0300

    Update App.jsx

--- react/React CSS/src/App.jsx
+++ react/React CSS/src/App.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import "./App.css";
 import MyComponent from "./components/MyComponent";
 
 function App() {
+  const n = 5;
+  const [name] = useState("Erik");
   return (
     <>
       <div>
@@ -16,6 +19,18 @@ function App() {
         >
           CSS INLINE
         </p>
+        {/* CSS INLINE DINÂMICO */}
+        <h2 style={n < 10 ? { color: "red" } : { color: "green" }}> {n}</h2>
+        <h2 style={n > 10 ? { color: "purple" } : { color: "green" }}>{n}</h2>
+        <h1
+          style={
+            name === "Erik"
+              ? { borderTop: "2px solid white", color: "gold" }
+              : null
+          }
+        >
+          {name}
+        </h1>
       </div>
     </>
   );
