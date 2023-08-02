f6a44a00102b28fcd296176a168ab8d840a620e4
Erik Ferreira <erikferreira03@outlook.com>
2023-08-02 19:24:38 -0300

    .

--- react/SecretWord/src/App.jsx
+++ react/SecretWord/src/App.jsx
@@ -1,14 +1,11 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
-    <>
-      
-    </>
-  )
+    <div className="index">
+      <h1>SECRET WORD</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
