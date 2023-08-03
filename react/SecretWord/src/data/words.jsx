9a1bf1d2f4b85804a55e9a7a9f4b5a35770fd6d9
Erik Ferreira <erikferreira03@outlook.com>
2023-08-03 20:09:53 -0300

    .

---
+++ react/SecretWord/src/data/words.jsx
@@ -0,0 +1,8 @@
+export const wordsList = {
+  carro: ["Motor", "Porta", "Antena", "Pneu", "Chassi"],
+  fruta: ["Manga", "Banana", "Acerola", "Melancia", "Laranja"],
+  corpo: ["Perna", "Peito", "Costas", "Olhos", "Ouvidos", "Panturrilha"],
+  computador: ["Placa de Video", "Gabinete", "Processador", "Teclado"],
+  programação: ["Linguagem", "Framework", "JavaScript", "Ruby", "React"],
+  alimento: ["Arroz", "Pamonha", "Lasanha", "Feijoada", "Cuscuz"],
+};
