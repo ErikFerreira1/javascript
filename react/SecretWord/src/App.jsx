3d8c15ebd6bfd56190c6f5e6c191afee962fe49f
Erik Ferreira <erikferreira03@outlook.com>
2023-08-04 20:14:17 -0300

    .

--- react/SecretWord/src/App.jsx
+++ react/SecretWord/src/App.jsx
@@ -1,10 +1,77 @@
+// CSS
 import "./App.css";
+// React
+import { useCallback, useEffect, useState } from "react";
+// Data
+import { wordsList } from "./data/words";
+// Components
 import StartScreen from "./components/StartScreen";
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndCatergory = () => {
+    // Pegar uma categoria
+    const categories = Object.keys(words);
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    console.log(category);
+    // pegar uma palavra
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
+
+    console.log(word);
+
+    return { word, category };
+  };
+  //começar jogo
+  const startGame = () => {
+    //pegar palavra e categoria
+
+    const { word, category } = pickWordAndCatergory();
+
+    //create an array of letters
+    let wordLetters = word.split("");
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    console.log(word, category);
+    console.log(wordLetters);
+
+    // Setar estados
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(letters);
+
+    setGameStage(stages[1].name);
+  };
+  //ir para o final do jogo
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
+  //resetar o jogo
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
   return (
     <div className="index">
-      <StartScreen/>
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "end" && <GameOver retry={retry} />}
     </div>
   );
 }
--- react/SecretWord/src/data/words.jsx
+++ react/SecretWord/src/data/words.jsx
@@ -2,7 +2,7 @@ export const wordsList = {
   carro: ["Motor", "Porta", "Antena", "Pneu", "Chassi"],
   fruta: ["Manga", "Banana", "Acerola", "Melancia", "Laranja"],
   corpo: ["Perna", "Peito", "Costas", "Olhos", "Ouvidos", "Panturrilha"],
-  computador: ["Placa de Video", "Gabinete", "Processador", "Teclado"],
+  computador: ["Memoria", "Gabinete", "Processador", "Teclado"],
   programação: ["Linguagem", "Framework", "JavaScript", "Ruby", "React"],
   alimento: ["Arroz", "Pamonha", "Lasanha", "Feijoada", "Cuscuz"],
 };
