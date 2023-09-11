ae77963eaa01826e211decec7bc66fabc0d22fd6
Erik Ferreira <erikferreira03@outlook.com>
2023-09-11 14:24:06 -0300

    .

---
+++ projetos/portifólio/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import Logo Nav from ".../assets/logos/Logo Nav.jpg"
+
+const NavBar = () => {
+  return (
+    <nav>
+        <img src="/LogoNav.jpg"/>
+        <h2>Home</h2>
+        <h2>Sobre</h2>
+        <h2>Projetos</h2>
+    </nav>
+  )
+}
+
+export default NavBar
