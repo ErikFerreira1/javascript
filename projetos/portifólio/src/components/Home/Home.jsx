501df659df6a9c84b573d9d2370a27530e22f6ce
Erik Ferreira <erikferreira03@outlook.com>
2023-09-13 20:57:57 -0300

    .

--- projetos/portifólio/src/components/Home/Home.jsx
+++ projetos/portifólio/src/components/Home/Home.jsx
@@ -3,14 +3,16 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="Home">
-      <p>OLÁ, MEU NOME É</p>
-      <h1>ERIK</h1>
-      <h1>FERREIRA</h1>
-      <h3>DESENVOLVEDOR FRONT END</h3>
+      <p className="Info1">OLÁ, MEU NOME É</p>
+      <h1 className="Info2">ERIK</h1>
+      <h1 className="Info3">FERREIRA</h1>
+      <h3 className="Info4">DESENVOLVEDOR </h3>
+      <h3 className="Info5">FRONT END</h3>
+
       <article className="Contato">
-        <img src="../public/logos/LogoGitHub.png" />
-        <img src="../public/logos/LogoLinkedin.png" />
-        <img src="../public/logos/LogoInstagram.png" />
+        <a href="https://github.com/ErikFerreira1" target="blank"><img src="../public/logos/LogoGitHub.png" /></a>
+        <a href="https://www.linkedin.com/feed/" target="blank"><img src="../public/logos/LogoLinkedin.png" /></a>
+        <a href="#"><img src="../public/logos/LogoEmail.png" /></a>
       </article>
     </div>
   );
