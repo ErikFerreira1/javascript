cda39b35e46c441bf3fc4be3bb8d9f88eb5b24f0
Erik Ferreira <erikferreira03@outlook.com>
2023-09-13 18:14:30 -0300

    .

--- projetos/portifólio/src/components/Home/Home.jsx
+++ projetos/portifólio/src/components/Home/Home.jsx
@@ -1,14 +1,19 @@
-import "./Home.css"
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>
-        <p>OLÁ, MEU NOME É</p>
-        <h1>ERIK</h1>
-        <h1>FERREIRA</h1>
-        <h3>DESENVOLVEDOR FRONT END</h3>
+    <div className="Home">
+      <p>OLÁ, MEU NOME É</p>
+      <h1>ERIK</h1>
+      <h1>FERREIRA</h1>
+      <h3>DESENVOLVEDOR FRONT END</h3>
+      <article className="Contato">
+        <img src="../public/logos/LogoGitHub.png" />
+        <img src="../public/logos/LogoLinkedin.png" />
+        <img src="../public/logos/LogoInstagram.png" />
+      </article>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
