a30d01bc793edf14570eef317ee38cea2170dfa6
Erik Ferreira <erikferreira03@outlook.com>
2023-09-16 23:38:51 -0300

    .

--- projetos/portifólio/src/components/Home/Home.jsx
+++ projetos/portifólio/src/components/Home/Home.jsx
@@ -3,16 +3,23 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="Home">
-      <p className="Info1">OLÁ, MEU NOME É  </p>
-      <h1 className="Info2">ERIK  </h1>
-      <h1 className="Info3">FERREIRA</h1>
-      <h3 className="Info4">DESENVOLVEDOR </h3>
-      <h3 className="Info5">FRONT END</h3>
+      <p className="Info1">OLÁ, MEU NOME É </p>
+      <h1 className="Info2">ERIK<b className="Info3"><br />FERREIRA</b><b className="Info4"><br />
+        DESENVOLVEDOR <b className="Info5"> FRONT-END </b></b>
+      </h1>
+
+      
 
       <article className="Contato">
-        <a href="https://github.com/ErikFerreira1" target="blank"><img src="../public/logos/LogoGitHub.png" /></a>
-        <a href="https://www.linkedin.com/feed/" target="blank"><img src="../public/logos/LogoLinkedin.png" /></a>
-        <a href="#"><img src="../public/logos/LogoEmail.png" /></a>
+        <a href="https://github.com/ErikFerreira1" target="blank">
+          <img src="../public/logos/LogoGitHub.png" />
+        </a>
+        <a href="https://www.linkedin.com/feed/" target="blank">
+          <img src="../public/logos/LogoLinkedin.png" />
+        </a>
+        <a href="#">
+          <img src="../public/logos/LogoEmail.png" />
+        </a>
       </article>
     </div>
   );
