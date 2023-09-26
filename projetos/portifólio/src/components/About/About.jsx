dc9b77b71e5d8c87263cfb8ae25e26881ec16ea7
Erik Ferreira <erikferreira03@outlook.com>
2023-09-25 22:12:28 -0300

    .

---
+++ projetos/portifólio/src/components/About/About.jsx
@@ -0,0 +1,23 @@
+import "./About.css";
+
+const About = () => {
+  return (
+    <div className="AboutMe">
+      <h1 className="About">SOBRE MIM</h1>
+      <h1 className="Skills">SKILLS</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi unde
+        deleniti amet incidunt possimus maiores dolorum quo? Fugiat accusantium,
+        facilis tenetur perferendis itaque natus impedit quibusdam accusamus,
+        voluptatem repudiandae ea?
+      </p>
+      <img src="../public/logos/LogoHTML5.png" />
+      <img src="../public/logos/LogoCSS3.png" />
+      <img src="../public/logos/LogoJavaScript.png" />
+      <img src="../public/logos/LogoReact.png" />
+      <img src="../public/logos/LogoPython.png" />
+    </div>
+  );
+};
+
+export default About;
--- projetos/portifólio/src/components/Home/Home.jsx
+++ projetos/portifólio/src/components/Home/Home.jsx
@@ -4,11 +4,17 @@ const Home = () => {
   return (
     <div className="Home">
       <p className="Info1">OLÁ, MEU NOME É </p>
-      <h1 className="Info2">ERIK<b className="Info3"><br />FERREIRA</b><b className="Info4"><br />
-        DESENVOLVEDOR <b className="Info5"> FRONT-END </b></b>
+      <h1 className="Info2">
+        ERIK
+        <b className="Info3">
+          <br />
+          FERREIRA
+        </b>
+        <b className="Info4">
+          <br />
+          DESENVOLVEDOR <b className="Info5"> FRONT-END </b>
+        </b>
       </h1>
-
-      
 
       <article className="Contato">
         <a href="https://github.com/ErikFerreira1" target="blank">
