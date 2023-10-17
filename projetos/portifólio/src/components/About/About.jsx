366a5324f1022e50c8fe43cb461ed17f93ca2a0e
Erik Ferreira <erikferreira03@outlook.com>
2023-10-17 20:26:49 -0300

    .

--- projetos/portifólio/src/components/About/About.jsx
+++ projetos/portifólio/src/components/About/About.jsx
@@ -4,39 +4,31 @@ const About = () => {
   return (
     <>
       <div className="AboutMe">
-        
         <h1 className="About">SOBRE MIM </h1>
-        <hr className="Separate_About"/>
+        <hr className="Separate_About" />
         <h1 className="NameSkills">SKILLS</h1>
-        <hr className="Separate_Skills"/>
-        
+        <hr className="Separate_Skills" />
+
         <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
           unde deleniti amet incidunt possimus maiores dolorum quo? Fugiat
           accusantium, facilis tenetur perferendis itaque natus impedit
-          quibusdam accusamus, voluptatem repudiandae ea?
-          lorem
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
-          unde deleniti amet incidunt possimus maiores dolorum quo? Fugiat
-          accusantium, facilis tenetur perferendis itaque natus impedit
-          quibusdam accusamus, voluptatem repudiandae ea?
-          lorem
+          quibusdam accusamus, voluptatem repudiandae ea? lorem Lorem ipsum,
+          dolor sit amet consectetur adipisicing elit. Excepturi unde deleniti
+          amet incidunt possimus maiores dolorum quo? Fugiat accusantium,
+          facilis tenetur perferendis itaque natus impedit quibusdam accusamus,
+          voluptatem repudiandae ea? lorem
+          
         </p>
-        <div className="Picture">
-          <img className="Eu" src="../test/Exemplo.png"  />
-          <div className="circle1"></div>
-          <div className="circle2"></div>
-          <div className="circle3"></div>
-        </div>
 
-        <div className="Skills" >
-        
-        <img className="Html5" src="../logos/LogoHTML5.png" />
-        <img className="Css3" src="../logos/LogoCSS3.png" />
-        <img className="JS" src="../logos/LogoJavaScript.png" />
-        <img className="React" src="../logos/LogoReact.png" />
-        <img className="Python" src="../logos/LogoPython.png" />
-        
+        <img className="Me" src="../test/Exemplo.png" />
+
+        <div className="Skills">
+          <img className="Html5" src="../logos/LogoHTML5.png" />
+          <img className="Css3" src="../logos/LogoCSS3.png" />
+          <img className="JS" src="../logos/LogoJavaScript.png" />
+          <img className="React" src="../logos/LogoReact.png" />
+          <img className="Python" src="../logos/LogoPython.png" />
         </div>
       </div>
     </>
