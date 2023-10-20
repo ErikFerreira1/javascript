3ed9577e29c0b6caf0f11305fab8c201a5544d83
Erik Ferreira <erikferreira03@outlook.com>
2023-10-20 19:26:14 -0300

    .

--- projetos/portifólio/src/components/Project/Project.jsx
+++ projetos/portifólio/src/components/Project/Project.jsx
@@ -1,23 +1,33 @@
+import "./Project.css";
 const Project = () => {
   return (
-    <div>
+    <div className="Project">
       <h1>PROJETO</h1>
-      <br className="Separate_Project" />
-      <img src="" />
-      <br className="Separate_Info"/>
-      <h2>E-COMMERCE</h2>
-      <h3>SOBRE O PROJETO</h3>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad neque
-        placeat consequuntur. Corrupti aut doloremque, excepturi pariatur
-        dignissimos deserunt provident unde delectus! Est, eum modi! Officiis,
-        dolorem officia. Esse, sequi.
-      </p>
-      <h3>TECNOLOGIAS USADAS</h3>
-      <img src="../logos/LogoJavaScript.png" />
-      <img src="../logos/LogoReact.png" />
-      <button>VISITAR</button>
-      <button>REPOSITORIO</button>
+      <hr className="Separate_Project" />
+
+      <img className="ImageProject" src="../imageProject/ModeloProjeto.png" />
+
+      <hr className="Separate_Info" />
+
+      <div className="Info_Project">
+        <h2>E-COMMERCE</h2>
+
+        <h3>SOBRE O PROJETO</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad neque
+          placeat consequuntur. Corrupti aut doloremque, excepturi pariatur
+          dignissimos deserunt provident unde delectus! Est, eum modi! Officiis,
+          dolorem officia. Esse, sequi.
+        </p>
+
+        <h4>TECNOLOGIAS USADAS</h4>
+        <img src="../logos/LogoJavaScript.png" />
+        <img src="../logos/LogoReact.png" />
+
+        <button>VISITAR</button>
+        <button>REPOSITORIO</button>
+      </div>
+
     </div>
   );
 };
